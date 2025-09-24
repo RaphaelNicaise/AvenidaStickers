a0c5be619d3f864b5258e09c7687de4c93aad7ad
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-24 00:53:51 -0300

    init pagina

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import paymentsRoutes from "./routes/payments";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Backend corriendo 🚀'));
+
+// Health check
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK'});
+});
+
+app.use("/payments", paymentsRoutes);
+
+app.get('/success', (req, res) => {
+  res.send('<h1>¡Pago exitoso!</h1>');
+});
+
+app.get('/failure', (req, res) => {
+  res.send('<h1>Pago fallido</h1>');
+});
+
+app.get('/pending', (req, res) => {
+  res.send('<h1>Pago pendiente</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server corriendo en http://localhost:${PORT}`);
+});
