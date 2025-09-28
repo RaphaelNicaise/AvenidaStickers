590ae454183d95cfe845268307bb855d92801247
RaphaelNicaise <raphanicaise@gmail.com>
2025-09-28 00:47:36 -0300

    Pagina

--- scripts/renew-ssl.sh
+++ scripts/renew-ssl.sh
@@ -13,7 +13,7 @@ cp /etc/letsencrypt/live/avenidastickers.me/fullchain.pem /var/www/avenidasticke
 cp /etc/letsencrypt/live/avenidastickers.me/privkey.pem /var/www/avenidastickers/ssl/
 
 # Permisos correctos
-chown avenida:avenida /var/www/avenidastickers/ssl/*
+chown root /var/www/avenidastickers/ssl/*
 chmod 644 /var/www/avenidastickers/ssl/fullchain.pem
 chmod 600 /var/www/avenidastickers/ssl/privkey.pem
 
